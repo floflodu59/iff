a1398bc492806113682beeeeeeef77e2b5b8f77c
floflodu59 <florent.fleuri@infosyscom.eu>
2024-11-19 09:29:15 +0100

    Update setupnetwork.sh

--- phase1/setupnetwork.sh
+++ phase1/setupnetwork.sh
@@ -4,13 +4,13 @@ echo "network:" >> /etc/netplan/00-installer-config.yaml
 echo " ethernets:" >> /etc/netplan/00-installer-config.yaml
 echo "  enp1s0:" >> /etc/netplan/00-installer-config.yaml
 echo "   dhcp4: no" >> /etc/netplan/00-installer-config.yaml
-echo "   addresses: [192.168.51.100/24]" >> /etc/netplan/00-installer-config.yaml
-echo "   gateway4: 192.168.51.254" >> /etc/netplan/00-installer-config.yaml
+echo "   addresses: [ip/mask]" >> /etc/netplan/00-installer-config.yaml
+echo "   gateway4: ip" >> /etc/netplan/00-installer-config.yaml
 echo "   nameservers:" >> /etc/netplan/00-installer-config.yaml
 echo "    addresses:" >> /etc/netplan/00-installer-config.yaml
 echo "    - 8.8.8.8" >> /etc/netplan/00-installer-config.yaml
 echo "    - 1.1.1.1" >> /etc/netplan/00-installer-config.yaml
-echo "    - 192.168.51.254" >> /etc/netplan/00-installer-config.yaml
+echo "    - ip" >> /etc/netplan/00-installer-config.yaml
 echo " version: 2" >> /etc/netplan/00-installer-config.yaml
 #echo " renderer: NetworkManager" >> /etc/netplan/00-installer-config.yaml
 netplan apply

83042d50941e270538e02032c7b63c7398a979a8
floflodu59 <florent.fleuri@infosyscom.eu>
2025-04-14 15:22:16 +0200

    Update errorhandler.sh

--- backup/scripts/errorhandler.sh
+++ backup/scripts/errorhandler.sh
@@ -13,6 +13,9 @@ sujet=""
 #echo $status
 #echo "${stamp}0"
 
+#Codes d'etat :
+# 0 - La fonction de sauvegarde au sein du script de s'est pas effectué
+# 1 - Sauvegarde complétée sans problèmes
 function sendmail
 {
 
@@ -23,6 +26,20 @@ function sendmail
                 echo "Bonjour," >> /backup/scripts/sendmail
                 echo "La sauvegarde du serveur ISIL du site ${site} n'a pas aboutie. Le script de sauvegarde ne s'est pas execute." >> /backup/scripts/sendmail #Changer ici au besoin
                 echo "Bonne journee," >> /backup/scripts/sendmail
+                echo "SFF 1.0" >> /backup/scripts/sendmail
+        fi
+	 if [ $status -eq 0 ] ; then
+		echo "Subject: [${sujet}][${site}] ECHEC - La sauvegarde des donnees ISIL ne s'est pas effectuee." >> /backup/scripts/sendmail #Changer ici au besoin
+		echo "Bonjour," >> /backup/scripts/sendmail
+		echo "La sauvegarde du serveur ISIL du site ${site} n'a pas aboutie." >> /backup/scripts/sendmail #Changer ici au besoin
+		echo "Bonne journee," >> /backup/scripts/sendmail
+		echo "SFF 1.0 - " >> /backup/scripts/sendmail
+        fi
+        if [ $status -eq 1 ] ; then
+		echo "Subject: [${sujet}][${site}] SUCCES - La sauvegarde des donnees ISIL s'est effectuee." >> /backup/scripts/sendmail #Changer ici au besoin
+		echo "Bonjour," >> /backup/scripts/sendmail
+		echo "La sauvegarde du serveur ISIL du site ${site} s'est effectuee correctement." >> /backup/scripts/sendmail #Changer ici au besoin
+		echo "Bonne journee," >> /backup/scripts/sendmail
                 echo "SFF 1.0" >> /backup/scripts/sendmail
         fi
         /usr/sbin/ssmtp $sender < /backup/scripts/sendmail

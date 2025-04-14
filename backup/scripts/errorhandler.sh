7aaddb46f816aa5016db659cefa24d6cb80f97d3
floflodu59 <florent.fleuri@infosyscom.eu>
2025-04-14 15:17:50 +0200

    Update errorhandler.sh

--- backup/scripts/errorhandler.sh
+++ backup/scripts/errorhandler.sh
@@ -13,22 +13,22 @@ sujet=""
 #echo $status
 #echo "${stamp}0"
 
-# function sendmail
-# {
+function sendmail
+{
 
-        # echo "To: ${recipients}" >> /backup/scripts/sendmail
-        # echo "From: ${sender}" >> /backup/scripts/sendmail
-        # if [ "$status" = "${stamp}5" ] ; then
-                # echo "Subject: [${sujet}][${site}] ECHEC - La sauvegarde des donnees ISIL ne s'est pas effectuee." >> /backup/scripts/sendmail #Changer ici au besoin
-                # echo "Bonjour," >> /backup/scripts/sendmail
-                # echo "La sauvegarde du serveur ISIL du site ${site} n'a pas aboutie. Le script de sauvegarde ne s'est pas execute." >> /backup/scripts/sendmail #Changer ici au besoin
-                # echo "Bonne journee," >> /backup/scripts/sendmail
-                # echo "SFF 1.0" >> /backup/scripts/sendmail
-        # fi
-        # /usr/sbin/ssmtp $sender < /backup/scripts/sendmail
-        # rm /backup/scripts/sendmail
-        # echo "${current_date} - Envoi du mail effectuÃ©." >> /backup/exportdb/history.log
-# }
+        echo "To: ${recipients}" >> /backup/scripts/sendmail
+        echo "From: ${sender}" >> /backup/scripts/sendmail
+        if [ "$status" = "${stamp}5" ] ; then
+                echo "Subject: [${sujet}][${site}] ECHEC - La sauvegarde des donnees ISIL ne s'est pas effectuee." >> /backup/scripts/sendmail #Changer ici au besoin
+                echo "Bonjour," >> /backup/scripts/sendmail
+                echo "La sauvegarde du serveur ISIL du site ${site} n'a pas aboutie. Le script de sauvegarde ne s'est pas execute." >> /backup/scripts/sendmail #Changer ici au besoin
+                echo "Bonne journee," >> /backup/scripts/sendmail
+                echo "SFF 1.0" >> /backup/scripts/sendmail
+        fi
+        /usr/sbin/ssmtp $sender < /backup/scripts/sendmail
+        rm /backup/scripts/sendmail
+        echo "${current_date} - Envoi du mail effectue." >> /backup/exportdb/history.log
+}
 
 
 function errorhandler

d8186eafdc2c0648d1488cd821b9e969d8b90643
floflodu59 <florent.fleuri@infosyscom.eu>
2025-04-15 11:54:49 +0200

    Update errorhandler.sh

--- backup/scripts/errorhandler.sh
+++ backup/scripts/errorhandler.sh
@@ -5,10 +5,10 @@ current_date=$(date +"%Y%m%d-%H%M%S")
 statusfile="/backup/scripts/status"
 status=$(cat "$statusfile")
 stamp=$(date +"%Y%m%d")
-sender=""
-recipients=""
-site=""
-sujet=""
+sender="sauvegardes@isc-group.fr"
+recipients="sauvegardes@isc-group.fr"
+site="IFF-02"
+sujet="TEST"
 
 #echo $status
 #echo "${stamp}0"
@@ -31,21 +31,21 @@ function sendmail
                 echo "Bonne journee," >> /backup/scripts/sendmail
                 echo "SFF 1.0" >> /backup/scripts/sendmail
         fi
-	 if [ $status -eq 0 ] ; then
+	 if [ $status -eq "${stamp}0" ] ; then
 		echo "Subject: [${sujet}][${site}] ECHEC - La sauvegarde des donnees ISIL ne s'est pas effectuee." >> /backup/scripts/sendmail #Changer ici au besoin
 		echo "Bonjour," >> /backup/scripts/sendmail
 		echo "La sauvegarde du serveur ISIL du site ${site} n'a pas aboutie correctement." >> /backup/scripts/sendmail #Changer ici au besoin
 		echo "Bonne journee," >> /backup/scripts/sendmail
 		echo "SFF 1.0 - " >> /backup/scripts/sendmail
         fi
-        if [ $status -eq 1 ] ; then
+        if [ $status -eq "${stamp}1" ] ; then
 		echo "Subject: [${sujet}][${site}] SUCCES - La sauvegarde des donnees ISIL s'est effectuee." >> /backup/scripts/sendmail #Changer ici au besoin
 		echo "Bonjour," >> /backup/scripts/sendmail
 		echo "La sauvegarde du serveur ISIL du site ${site} s'est effectuee correctement." >> /backup/scripts/sendmail #Changer ici au besoin
 		echo "Bonne journee," >> /backup/scripts/sendmail
                 echo "SFF 1.0" >> /backup/scripts/sendmail
         fi
-	if [ $status -eq 2 ] ; then
+	if [ $status -eq "${stamp}2" ] ; then
 		echo "Subject: [${sujet}][${site}] SUCCES - La sauvegarde des donnees ISIL s'est effectuee." >> /backup/scripts/sendmail #Changer ici au besoin
 		echo "Bonjour," >> /backup/scripts/sendmail
 		echo "La sauvegarde du serveur ISIL du site ${site} n'a pas aboutie correctement en raison d'un ou plusieurs des composants de sauvegarde qui ne se sont pas execute correctement." >> /backup/scripts/sendmail
@@ -53,7 +53,7 @@ function sendmail
                 echo "SFF 1.0" >> /backup/scripts/sendmail
         fi
         /usr/sbin/ssmtp $sender < /backup/scripts/sendmail
-        rm /backup/scripts/sendmail
+        #rm /backup/scripts/sendmail
         echo "${current_date} - Envoi du mail effectue." >> /backup/exportdb/history.log
 }
 
@@ -74,4 +74,4 @@ function errorhandler
 }
 
 errorhandler
-
+sendmail
